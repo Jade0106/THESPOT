db5571ef55226dd436d0c5989cb62749aeaa8c16
Jade0106 <jadelangeveld@gmail.com>
2023-05-30 00:22:40 +0200

    updated DeptEmployee View and Controller

--- THE_SPOT/Controllers/DeptEmployeeController.cs
+++ THE_SPOT/Controllers/DeptEmployeeController.cs
@@ -39,18 +39,18 @@ namespace THE_SPOT.Controllers
         {
             return View();
         }
-        public IActionResult PRHistoryDepartments()
+        public IActionResult CoffeePRHistory()
         {
             IEnumerable<CoffeeDeptPR> objList = _db.CoffeeDeptPR;
             return View(objList);
         }
-        public IActionResult Create()
+        public IActionResult CoffeeCreate()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(CoffeeDeptPR obj)
+        public IActionResult CoffeeCreate(CoffeeDeptPR obj)
         {
             if (ModelState.IsValid)
             {
@@ -61,7 +61,7 @@ namespace THE_SPOT.Controllers
             return View(obj);
         }
 
-        public IActionResult Update(int? id)
+        public IActionResult CoffeeUpdate(int? id)
         {
             if (id == null || id == 0)
             {
@@ -80,14 +80,14 @@ namespace THE_SPOT.Controllers
         //POST-Update updating the current data we have 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update(CoffeeDeptPR obj)
+        public IActionResult CoffeeUpdate(CoffeeDeptPR obj)
         {
             _db.CoffeeDeptPR.Update(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int? id)
+        public IActionResult CoffeeDelete(int? id)
         {
 
             deleteId = id;
@@ -106,13 +106,183 @@ namespace THE_SPOT.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult DeletePost(int? id)
+        public IActionResult CoffeeDeletePost(int? id)
         {
             var obj = _db.CoffeeDeptPR.Find(deleteId);
 
             try
             {
                 _db.CoffeeDeptPR.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult TeaPRHistory()
+        {
+            IEnumerable<TeaDeptPR> objList = _db.TeaDeptPR;
+            return View(objList);
+        }
+        public IActionResult TeaCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult TeaCreate(TeaDeptPR obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.TeaDeptPR.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult TeaUpdate(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.TeaDeptPR.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST-Update updating the current data we have 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult TeaUpdate(TeaDeptPR obj)
+        {
+            _db.TeaDeptPR.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult TeaDelete(int? id)
+        {
+
+            deleteId = id;
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.TeaDeptPR.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult TeaDeletePost(int? id)
+        {
+            var obj = _db.TeaDeptPR.Find(deleteId);
+
+            try
+            {
+                _db.TeaDeptPR.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult MugsPRHistory()
+        {
+            IEnumerable<MugsDeptPR> objList = _db.MugsDeptPR;
+            return View(objList);
+        }
+        public IActionResult MugsCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MugsCreate(MugsDeptPR obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.MugsDeptPR.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult MugsUpdate(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.MugsDeptPR.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST-Update updating the current data we have 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MugsUpdate(MugsDeptPR obj)
+        {
+            _db.MugsDeptPR.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult MugsDelete(int? id)
+        {
+
+            deleteId = id;
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.MugsDeptPR.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MugsDeletePost(int? id)
+        {
+            var obj = _db.MugsDeptPR.Find(deleteId);
+
+            try
+            {
+                _db.MugsDeptPR.Remove(obj);
                 _db.SaveChanges();
             }
             catch (DbUpdateException ex)
