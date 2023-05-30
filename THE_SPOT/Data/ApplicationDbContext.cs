f6c08d5f656ec4d09ef37a49749147d946dd013e
Anna33214 <aredlinghuys@gmail.com>
2023-05-30 15:19:19 +0200

    Added DbSet- Tea

--- THE_SPOT/Data/ApplicationDbContext.cs
+++ THE_SPOT/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using THE_SPOT.Models;
 
@@ -10,9 +11,24 @@ namespace THE_SPOT.Data
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Tea>()
+       .Property(p => p.Price)
+       .HasPrecision(18, 2);
+            modelBuilder.Entity<IdentityUserLogin<string>>()
+         .HasKey(l => new { l.LoginProvider, l.ProviderKey });
+
+            modelBuilder.Entity<IdentityUserRole<string>>()
+                .HasKey(r => new { r.UserId, r.RoleId });
+            modelBuilder.Entity<IdentityUserToken<string>>()
+        .HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+        }
         public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<CoffeeDeptPR> CoffeeDeptPR { get; set; }
         public DbSet<TeaDeptPR> TeaDeptPR { get; set; }
         public DbSet<MugsDeptPR> MugsDeptPR { get; set; }
+        public DbSet<Tea> Teas { get; set; }
     }
 }
