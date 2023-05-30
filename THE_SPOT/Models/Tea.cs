d4be2c7e571db1153ef6d15397b1e168b782ca9f
Anna33214 <aredlinghuys@gmail.com>
2023-05-30 15:20:02 +0200

    Added Tea and TeaView Models

---
+++ THE_SPOT/Models/Tea.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace THE_SPOT.Models
+{
+    public class Tea
+    {
+        [Key]
+        public int TeaId { get; set; }
+
+        [Required(ErrorMessage = "Please provide the product name.")]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please provide the product description.")]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please provide the product price.")]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please provide the product image.")]
+        [DisplayName("Image")]
+        public byte[] Image { get; set; }
+
+        [NotMapped] // Exclude from database
+        [Required(ErrorMessage = "Please provide the product image.")]
+        [DisplayName("Image")]
+        public IFormFile ImageFile { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+
+   
+}
+
