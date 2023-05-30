c8bbafc7b25da5169036fec64a9ceb5a7fafa969
Jade0106 <jadelangeveld@gmail.com>
2023-05-30 14:03:52 +0200

    purchase request status fixed

--- THE_SPOT/Models/CoffeeDeptPR.cs
+++ THE_SPOT/Models/CoffeeDeptPR.cs
@@ -27,8 +27,9 @@ namespace THE_SPOT.Models
         [DisplayName("Total")]
         public float total { get; set; }
 
-        [DisplayName("Request Status")]
-        public string status { get; set; }
-        
+    }
+    public enum coffeePRStatus
+    {
+        Pending
     }
 }
